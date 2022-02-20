df6546575b5e12a8a0b324fcaba9d0f71aec9f21
salalo <wojteksala190@gmail.com>
2022-02-20 15:53:12 +0100

    simplify rust code

--- lib/rewasm.ex
+++ lib/rewasm.ex
@@ -20,11 +20,9 @@ end
 defmodule REWASM.NIF.Base64 do
   use Rustler, otp_app: :rewasm, crate: "rewasm_nif_base64"
 
-  @spec decode(binary, atom) :: binary
-  def decode(_b64, _opt \\ :standard), do: error()
+  def decode(), do: error()
 
-  @spec encode(binary, atom) :: binary
-  def encode(_s, _opt \\ :standard), do: error()
+  def encode(), do: error()
 
   defp error(), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/rewasm_nif_base64/src/lib.rs
+++ native/rewasm_nif_base64/src/lib.rs
@@ -1,30 +1,17 @@
 use base64;
-use rustler::Atom;
+use std::str;
 
-mod atoms {
-    rustler::atoms! {
-      crypt,
-      imap_map7,
-      standard,
-      standard_no_pad,
-      url_safe,
-      url_safe_no_pad,
-    }
+#[rustler::nif]
+pub fn decode() -> String {
+    let encoded = String::from("Q2FuIHlvdSBwbGVhc2UKU2l0IHRoZSBmdWNrIGRvd24KUHJvdGVzdGluZyBpbiB5b3VyClBhcGVyIGNyb3duCllvdSBsb3ZlIHRvIGZlZWwgb2ZmZW5kZWQKRmlnaHRpbmcgZnJvbSBjb21wdXRlciB0cmVuY2hlcwoKW0Nob3J1c10KWW91IGdvdCBhIHNlbWktYXV0b21hdGljIG1vdXRoCkdvIGVhc3kgbm93CkFuZCB5b3UncmUgc28gbG91ZApBbmQgeW91J3JlIHNvIGxvdWQKCltWZXJzZSAyXQpFdmVyeW9uZSdzIHllbGxpbmcKSXQncyBjcmFja2luZyB0aGUgY2VpbGluZwpQYWludCBwZWVsaW5nCkxpa2UgYmFuYW5hIGZsb29yIHNwbGl0dGluZwpXb29kIHNwbGludGVyaW5nClRoZSBub2lzZSBpcyBkZWFmZW5pbmcKUmluZyB0aGUgYWxhcm0gYW5vdGhlciBzb3VuZApJcyBjb3B1bGF0aW5nClNwcmVhZCBlYWdsZSBvbiBteSBlYXJkcnVtcwpDaG9raW5nIG9uIFR3aXR0ZXIgY3VtCkkgbWlzcyBzdW1tZXIgZG9sZHJ1bXMKSSBtaXNzIHNvbmljIGJvcmVkb20KSSBNaXNzIENvbmdlbmlhbGl0eQpTdHJhbmdsZWQgaW5jZXNzYW50bHkKQnkgdGhlIHNwZWFrZXIgaHVtClNjcmVhbWluZyBtb25rcwpFYXJzIGFyZSBudW1iCkdvZCBjYW4ndCB3aGlzcGVyCldoZW4gdGhlIGJhc3MgaXMgdXAKW0Nob3J1c10KCltWZXJzZSAzXQpDYW4geW91IHBsZWFzZQpUdXJuIHlvdXJzZWxmIGRvd24KUml0YWxpbiBraWRzCkRvaW5nIGNvY2FpbmUgd2lwZSBvdXRzCk5vIG5lZWQgdG8gZmVlbCBvZmZlbmRlZApCb3JuIHRvIGEgdGltZQpXaGVuIHRoZSBxdWlldCBlbmRlZAoKW0Nob3J1c10K");
+    let decoded = base64::decode(&encoded).unwrap();
+
+    str::from_utf8(&decoded[..]).unwrap().to_string()
 }
 
 #[rustler::nif]
-pub fn decode(opt: Atom) -> String {
-    let txt = String::from("Q2FuIHlvdSBwbGVhc2UKU2l0IHRoZSBmdWNrIGRvd24KUHJvdGVzdGluZyBpbiB5b3VyClBhcGVyIGNyb3duCllvdSBsb3ZlIHRvIGZlZWwgb2ZmZW5kZWQKRmlnaHRpbmcgZnJvbSBjb21wdXRlciB0cmVuY2hlcwoKW0Nob3J1c10KWW91IGdvdCBhIHNlbWktYXV0b21hdGljIG1vdXRoCkdvIGVhc3kgbm93CkFuZCB5b3UncmUgc28gbG91ZApBbmQgeW91J3JlIHNvIGxvdWQKCltWZXJzZSAyXQpFdmVyeW9uZSdzIHllbGxpbmcKSXQncyBjcmFja2luZyB0aGUgY2VpbGluZwpQYWludCBwZWVsaW5nCkxpa2UgYmFuYW5hIGZsb29yIHNwbGl0dGluZwpXb29kIHNwbGludGVyaW5nClRoZSBub2lzZSBpcyBkZWFmZW5pbmcKUmluZyB0aGUgYWxhcm0gYW5vdGhlciBzb3VuZApJcyBjb3B1bGF0aW5nClNwcmVhZCBlYWdsZSBvbiBteSBlYXJkcnVtcwpDaG9raW5nIG9uIFR3aXR0ZXIgY3VtCkkgbWlzcyBzdW1tZXIgZG9sZHJ1bXMKSSBtaXNzIHNvbmljIGJvcmVkb20KSSBNaXNzIENvbmdlbmlhbGl0eQpTdHJhbmdsZWQgaW5jZXNzYW50bHkKQnkgdGhlIHNwZWFrZXIgaHVtClNjcmVhbWluZyBtb25rcwpFYXJzIGFyZSBudW1iCkdvZCBjYW4ndCB3aGlzcGVyCldoZW4gdGhlIGJhc3MgaXMgdXAKW0Nob3J1c10KCltWZXJzZSAzXQpDYW4geW91IHBsZWFzZQpUdXJuIHlvdXJzZWxmIGRvd24KUml0YWxpbiBraWRzCkRvaW5nIGNvY2FpbmUgd2lwZSBvdXRzCk5vIG5lZWQgdG8gZmVlbCBvZmZlbmRlZApCb3JuIHRvIGEgdGltZQpXaGVuIHRoZSBxdWlldCBlbmRlZAoKW0Nob3J1c10K");
-    let config: base64::Config = match_config(opt);
-    let bytes = base64::decode_config(txt, config).expect("decode failed: invalid b64");
-
-    String::from_utf8(bytes).unwrap()
-}
-
-#[rustler::nif]
-pub fn encode(opt: Atom) -> String {
-    let txt = String::from(
-        "Can you please
+pub fn encode() -> String {
+    let txt = b"Can you please
 Sit the fuck down
 Protesting in your
 Paper crown
@@ -69,26 +56,10 @@ Born to a time
 When the quiet ended
 
 [Chorus]
-",
-    );
-    let config: base64::Config = match_config(opt);
-    base64::encode_config(txt.as_bytes(), config)
-}
+";
+    let encoded = base64::encode(txt);
 
-fn match_config(option: Atom) -> base64::Config {
-    if option == atoms::crypt() {
-        base64::CRYPT
-    } else if option == atoms::imap_map7() {
-        base64::IMAP_MUTF7
-    } else if option == atoms::url_safe() {
-        base64::URL_SAFE
-    } else if option == atoms::url_safe_no_pad() {
-        base64::URL_SAFE_NO_PAD
-    } else if option == atoms::standard_no_pad() {
-        base64::STANDARD_NO_PAD
-    } else {
-        base64::STANDARD
-    }
+    encoded
 }
 
 rustler::init!("Elixir.REWASM.NIF.Base64", [decode, encode]);
