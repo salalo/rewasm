9afc05d694a6fc5e340f16146a06b2f5be9ab2ea
salalo <wojteksala190@gmail.com>
2022-02-20 14:19:21 +0100

    base64 rust nif

--- lib/rewasm.ex
+++ lib/rewasm.ex
@@ -16,3 +16,15 @@ defmodule REWASM do
     :world
   end
 end
+
+defmodule REWASM.NIF.Base64 do
+  use Rustler, otp_app: :rewasm, crate: "rewasm_nif_base64"
+
+  @spec decode(binary, atom) :: binary
+  def decode(_b64, _opt \\ :standard), do: error()
+
+  @spec encode(binary, atom) :: binary
+  def encode(_s, _opt \\ :standard), do: error()
+
+  defp error(), do: :erlang.nif_error(:nif_not_loaded)
+end
---
+++ native/rewasm_nif_base64/src/lib.rs
@@ -0,0 +1,45 @@
+use base64;
+use rustler::Atom;
+
+mod atoms {
+    rustler::atoms! {
+      crypt,
+      imap_map7,
+      standard,
+      standard_no_pad,
+      url_safe,
+      url_safe_no_pad,
+    }
+}
+
+#[rustler::nif]
+pub fn decode(b64: String, opt: Atom) -> String {
+    let config: base64::Config = match_config(opt);
+    let bytes = base64::decode_config(b64, config).expect("decode failed: invalid b64");
+
+    String::from_utf8(bytes).unwrap()
+}
+
+#[rustler::nif]
+pub fn encode(s: String, opt: Atom) -> String {
+    let config: base64::Config = match_config(opt);
+    base64::encode_config(s.as_bytes(), config)
+}
+
+fn match_config(option: Atom) -> base64::Config {
+    if option == atoms::crypt() {
+        base64::CRYPT
+    } else if option == atoms::imap_map7() {
+        base64::IMAP_MUTF7
+    } else if option == atoms::url_safe() {
+        base64::URL_SAFE
+    } else if option == atoms::url_safe_no_pad() {
+        base64::URL_SAFE_NO_PAD
+    } else if option == atoms::standard_no_pad() {
+        base64::STANDARD_NO_PAD
+    } else {
+        base64::STANDARD
+    }
+}
+
+rustler::init!("Elixir.REWASM.NIF.Base64", [decode, encode]);
